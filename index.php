41ec0faa9f68333799532ee3055ce5603d1e5a39
sudiksha711 <sudiksha711@gmail.com>
2020-06-16 14:17:38 -0400

    edit skills

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
 
 				  			<div class="more animate-intro">
 				  				<a class="smoothscroll button stroke" href="#about">
-				  					Contact me
+				  					Contact Me
 				  				</a>
 				  			</div>
 
